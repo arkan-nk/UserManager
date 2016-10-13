9091c9c920a91eab225acba7954dcb08c5e1c061
Arkan_nk <arkan.nk@gmail.com>
2016-10-13 14:32:30 +0300

    fix

--- src/main/java/ru/chipn/usermanage/login/SecurityInitializer.java
+++ src/main/java/ru/chipn/usermanage/login/SecurityInitializer.java
@@ -1,23 +1,11 @@
 package ru.chipn.usermanage.login;
 
 import org.picketlink.idm.PartitionManager;
-import org.picketlink.idm.config.IdentityConfiguration;
-import org.picketlink.idm.config.IdentityConfigurationBuilder;
-import org.picketlink.idm.credential.handler.PasswordCredentialHandler;
-import org.picketlink.idm.internal.DefaultPartitionManager;
-import org.picketlink.idm.model.IdentityType;
-import org.picketlink.idm.model.basic.*;
-import ru.chipn.usermanage.idm.AppBean;
-import ru.chipn.usermanage.idm.LDAPATTRS;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
-import java.util.List;
-
-import static org.picketlink.common.constants.LDAPConstants.*;
-import static ru.chipn.usermanage.login.ConfigurationEnum.*;
 
 /**
  * Created by arkan on 30.08.2016.
